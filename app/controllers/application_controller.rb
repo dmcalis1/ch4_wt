43b89a0995fc29bf4c0d47ca4810b63d6decd6c1
David <dmcalis1@uncc.edu>
2017-02-24 17:14:16 +0000

    finished through 4.4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
   protect_from_forgery with: :exception
 end
